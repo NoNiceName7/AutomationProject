cd45ef53c6e2247fdc04d95a1f0b2bdba94eb277
NoNiceName7 <121935358+NoNiceName7@users.noreply.github.com>
2023-03-01 16:34:28 +0200

    t

--- AOS_Classes/AOS_Speakers_Page.py
+++ AOS_Classes/AOS_Speakers_Page.py
@@ -5,13 +5,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class AOS_Main_Page:
+class AOS_Speakers_Page:
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
 
-    def categories_options(self):
-        return self.driver.find_elements(By.CSS_SELECTOR, ".container>.rowSection>.categoryCell")
+    def speaker_type(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, "ul>li[class='ng-scope']>img")
 
-    def categories_list(self,number):
-        return self.categories_options()[number-1]
-    
+    def speaker_list(self, number):
+        return self.speaker_type()[number - 1]
