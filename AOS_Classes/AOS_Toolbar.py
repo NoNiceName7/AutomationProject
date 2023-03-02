c2bd25147cc9a726435566f51fcd1caddef98ca9
ezraz3 <ezrazeit3@gmail.com>
2023-03-02 10:59:08 +0200

    ezra

--- AOS_Classes/AOS_Toolbar.py
+++ AOS_Classes/AOS_Toolbar.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class AOS_Toolbar:
@@ -11,3 +12,10 @@ class AOS_Toolbar:
 
     def returning_main_page(self):
         self.driver.find_element(By.CSS_SELECTOR,".logo>a").click()
+
+    def small_window(self):
+        shopping_cart_icon = self.driver.find_element(By.ID, "shoppingCartLink")
+        ActionChains(self.driver).move_to_element(shopping_cart_icon).perform()
+        small_window = self.driver.find_element(By.CSS_SELECTOR, "ul>li>#toolTipCart>div>table>tfoot>tr>td>span>label")
+        total = small_window.text
+        return total
